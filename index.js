4b952add03a72480db35b5dda13aaa0a3fd2ed3d
kelseylemmer <116584777+kelseylemmer@users.noreply.github.com>
2022-12-16 21:01:01 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// var header = document.querySelectorAll('.header')[0];
+
+// header.style.color = '#f00';
+
+// get all details elements from inside work experience card (add an id to the card containing all of my work experience)
+// loop through all of those elements and apply the "open" html attribute to each of them
+
+var x = document.getElementById("workExperience").open;
+
+function myFunction() {
+    document.getElementById("workExperience").open = true;
+}
