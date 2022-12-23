dc8b67f881d929db00d38db4f14d0081cd028e7e
kelseylemmer <116584777+kelseylemmer@users.noreply.github.com>
2022-12-23 10:20:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-// var header = document.querySelectorAll('.header')[0];
+var allDetails = document.querySelectorAll('#workExperience details');
 
-// header.style.color = '#f00';
-
-// get all details elements from inside work experience card (add an id to the card containing all of my work experience)
-// loop through all of those elements and apply the "open" html attribute to each of them
-
-var x = document.getElementById("workExperience").open;
-
-function myFunction() {
-    document.getElementById("workExperience").open = true;
+for(var i = 0; i < allDetails.length; i++) {
+    var currentDetail = allDetails[i];
+    currentDetail.open = true;    
 }
